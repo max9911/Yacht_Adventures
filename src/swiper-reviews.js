714ea627c12224d1ac9bc30d2e0251bff3158df0
JuliaDzimina <132845748+JuliaDzimina@users.noreply.github.com>
2023-07-29 19:54:11 +0300

    upg

---
+++ src/swiper-reviews.js
@@ -0,0 +1,44 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+   
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  
+    // Default parameters
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 768px
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      // when window width is >= 1280px
+      1280: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+      
+    }
+  
+  
+    
+  });
+  
+  
